5e5dd0ebde4ad362299d2d2c27d5dfc0001be52c
FabianFerreyra40 <leonardomarcospatri@gmail.com>
2023-12-26 19:35:20 -0300

    commit

---
+++ apps/usuario/forms.py
@@ -0,0 +1,48 @@
+'''from .models import Usuario
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth import authenticate, login
+
+class RegistroUsuarioForm(UserCreationForm):
+    
+    class Meta:
+        model = Usuario
+        fields = ['username', 'first_name', 'last_name', 'password1', 'password2', 'email', 'imagen']
+
+
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(label= 'Nombre de usuario')
+    password = forms.CharField(label='Contraseña', widget=forms.PasswordInput)
+
+    def login(self, request):
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)'''
+
+from .models import Usuario
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth import authenticate, login as auth_login
+
+class RegistroUsuarioForm(UserCreationForm):
+    
+    class Meta:
+        model = Usuario
+        fields = ['username', 'first_name', 'last_name', 'password1', 'password2', 'email', 'imagen']
+
+class LoginForm(forms.Form):
+    username = forms.CharField(label='Nombre de usuario')
+    password = forms.CharField(label='Contraseña', widget=forms.PasswordInput)
+
+    def process_login(self, request):
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user:
+            auth_login(request, user)
+
+
